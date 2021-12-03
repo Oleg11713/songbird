c296aef09c73e666ac732f1c17d3fc89fa604985
Oleg <ovchinskiy00@mail.ru>
2021-12-03 20:57:45 +0300

    feat: added header component and links to home, shop page and contact

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./homepage.style.scss";
+import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = () => (
